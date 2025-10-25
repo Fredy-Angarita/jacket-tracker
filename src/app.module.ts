478c8bb00dabbf7355efccdabbf511f7b3d4bc78
Fredy Angarita <frediangarita1999@gmail.com>
2025-10-25 13:43:54 -0500

    fix (app): delete unused endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './infrastructure/out/database/database.module';
 import { UserController } from './infrastructure/in/http/user.controller';
@@ -9,7 +8,7 @@ import { UserRepository } from './infrastructure/out/database/repository/user.re
 
 @Module({
   imports: [DatabaseModule],
-  controllers: [AppController, UserController],
+  controllers: [UserController],
   providers: [
     AppService,
     UserHandler,
